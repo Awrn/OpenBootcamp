a10e87dcc834a096182b7c84419d92e9cd3ec0d1
Awrn <alvarowrn97@gmail.com>
2022-03-18 09:47:12 +0100

    JavaBasico 1

---
+++ JavaBasico/Ejercicio1/src/com/alvarowagner/main/Main2.java
@@ -0,0 +1,28 @@
+package com.alvarowagner.main;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        //Para este primer ejercicio tendréis que realizar lo siguiente:
+        //Crea un proyecto de Java desde 0
+        //Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+        //Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+        //Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+
+
+        int num1 =  522322323;
+        float num2 = 512312323.522f;
+        long num3 = 3121131231232342L;
+        double num4 = 3123212123223233.12321213212;
+        boolean isit = true;
+        String nombre = "Ekko";
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(isit);
+        System.out.println(nombre);
+
+    }
+}
