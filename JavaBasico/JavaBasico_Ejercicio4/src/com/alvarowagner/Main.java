ade502aeb37173d567972f5bd1404a7ba22024e9
Awrn <alvarowrn97@gmail.com>
2022-03-21 13:26:55 +0100

    ejercicio4v2

--- JavaBasico/JavaBasico_Ejercicio4/src/com/alvarowagner/Main.java
+++ JavaBasico/JavaBasico_Ejercicio4/src/com/alvarowagner/Main.java
@@ -7,20 +7,28 @@ public class Main {
 
         DeviceScreen samsungScreen = new DeviceScreen("Samsung",20,29,"Amoled","2k");
         DeviceScreen indieTechScreen = new DeviceScreen("IndieTech",18,25,"Amoled","1080p");
+        DeviceScreen indieTechScreenMini = new DeviceScreen("IndieTech",10,15,"LED","480p");
 
-        SmartPhone iphone13S = new SmartPhone("Apple","Iphone11S",3200,true,true,samsungScreen,"Bits",false);
+        SmartPhone iphone13 = new SmartPhone("Apple","Iphone 13",3200,true,true,samsungScreen,"Bits",false);
         SmartPhone xiamoiMI9T = new SmartPhone("Xiaomi","MI9T",3500,true,false,indieTechScreen,"Xiaomi",false);
 
-        System.out.println(iphone13S);
-
-        iphone13S.screen.brand = "-----";
-        iphone13S.has5G = false;
-        iphone13S.batterycapacity = 3900;
+        SmartWatch iWatch5Pro = new SmartWatch ("Apple","iWatch5 Pro",1250,false,false,indieTechScreenMini,"Leather",17);
 
 
-        System.out.println(iphone13S);
+
+        System.out.println(iphone13);
+
+        iphone13.model = "Iphone 13S";
+        iphone13.screen.brand = "-----";
+        iphone13.has5G = false;
+        iphone13.batterycapacity = 3900;
+
+
+        System.out.println(iphone13);
 
         System.out.println(xiamoiMI9T);
+
+        System.out.println(iWatch5Pro);
 
     }
 }
